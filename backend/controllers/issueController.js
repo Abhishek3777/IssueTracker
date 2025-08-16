033fd218415c8958baa98d733b60ffe62ff3acf0
Abhishek3777 <kr7abhishek04@gmail.com>
2025-08-16 15:52:40 +0530

    fast fix

--- backend/controllers/issueController.js
+++ backend/controllers/issueController.js
@@ -36,24 +36,19 @@ exports.createIssue = async (req, res) => {
 //get all issues
 
 exports.getAllIssues = async (req, res) => {
-    const user = req.user;
-    console.log(user);
+   
 
     try {
-        const currUser = await User.findById(user._id);
-
-        if (!currUser) {
-            return res.status(404).json({ error: "User not found" });
-        }
         
-        if (currUser.role === 'worker') {
-            const issuesForWorker = await Issue.find({ assignedTo: user })
-                .populate("assignedTo", "name email")
-                .sort({ createdDate: -1 });
+        
+        // if (currUser.role === 'worker') {
+        //     const issuesForWorker = await Issue.find({ assignedTo: user })
+        //         .populate("assignedTo", "name email")
+        //         .sort({ createdDate: -1 });
 
 
-            return res.json(issuesForWorker);
-        }
+        //     return res.json(issuesForWorker);
+        // }
         const issues = await Issue.find()
             .populate('assignedTo', 'name email') // populate only needed fields
             .sort({ createdDate: -1 });
